eb3d1c4929ebfade563f426f09a6ad12710bf65c
SoSo505 <53016398+SoSo505@users.noreply.github.com>
2021-03-20 13:01:58 +0600

    Update urls.py

--- midterm/TODO/urls.py
+++ midterm/TODO/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('main/', include('main.urls')),
     path('auth/', include('auth_.urls'))
 ]
